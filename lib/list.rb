cc99fd90cd2419cb09cc9915a47fa3b78f5c110f
Amanda Lemmons <amanda.lemmons@gmail.com>
2016-12-05 18:52:24 +0100

    remove array from initialize

--- lib/list.rb
+++ lib/list.rb
@@ -9,26 +9,14 @@ class List
     @list = nil
     @count = 0
     @sort_order = options[:sorted]
-    # @lists = []
   end
 
   # add to end of the list
   def add(object)
     node = Node.new(object)
     node.object = object
+    # add in the correct sort order
 
-    # if @count == 0
-    #   @head = node
-    #   @tail = node
-    #   node.next = nil
-    # else
-    #   @tail.next = node
-    #   @tail = @tail.next
-    #   @tail.next = nil
-    # end
-    # @lists << node
-
-    # add in the correct sort order (haven't been able to put the method together/ check them. )
       # checks to see if the list is a sorted list or not / if there is a list
 
       if @count == 0
@@ -39,13 +27,10 @@ class List
         @tail.next = node
         @tail = @tail.next
         @tail.next = nil
-      # end
       elsif (@sort_order != 0 ) && (@count > 0)
-
         first = @head
         second = @head.next
         new_node = node
-
         # only one item in list and new item goes last
         if (first.object < new_node.object) && (second == nil)
          first.next = new_node
@@ -55,7 +40,7 @@ class List
         elsif (first.object > new_node.object)
           @head = new_node
           @head.next = first
-        # this works if the new_node goes between two exisiting nodes (ex[9, 11] --> wanting to add 10)
+        # new_node goes between two exisiting nodes (ex[9, 11] --> wanting to add 10)
         else
           while second != nil
             until first.object > new_node.object
@@ -69,7 +54,6 @@ class List
       end
     end
     @count += 1
-
   end
 
   # enumerable mixin method
@@ -89,17 +73,11 @@ class List
       current = current.next
     end
     elements << current.object
-    # self.head.object
-    # if @sort_order == 0 || @sort_order == nil
-    #   to_a
-    # else
-    #   to_a.sort
-    # end
   end
 
   # returns true if the list is empty, false otherwise
   def empty?
-    if to_a.empty?
+    if elements.empty?
       true
     else
       false
@@ -113,7 +91,7 @@ class List
 
   # returns true if object is a member of the list, false otherwise
   def member?(object)
-    if to_a.include?(object)
+    if elements.include?(object)
       true
     else
       false
@@ -123,9 +101,6 @@ class List
   # returns and removes the first element of the list; or nil if the list is empty
   def pop
     return nil if @count < 1
-      # remove the element from the array
-      @lists.shift
-
       # remove the first element and set second element to first position
       pop = @head.object
       @head = @head.next
