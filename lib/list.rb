4263e91a800040282e166194810117a0c68a0dd5
Amanda Lemmons <amanda.lemmons@gmail.com>
2016-12-05 18:42:02 +0100

    add new element in sorted order

--- lib/list.rb
+++ lib/list.rb
@@ -6,9 +6,10 @@ class List
   def initialize(options={})
     @head = nil
     @tail = nil
+    @list = nil
     @count = 0
     @sort_order = options[:sorted]
-    @lists = []
+    # @lists = []
   end
 
   # add to end of the list
@@ -16,59 +17,84 @@ class List
     node = Node.new(object)
     node.object = object
 
-    if @count == 0
-      @head = node
-      @tail = node
-      node.next = nil
-    else
-      @tail.next = node
-      @tail = @tail.next
-      @tail.next = nil
-    end
-    @count += 1
-    @lists << node
+    # if @count == 0
+    #   @head = node
+    #   @tail = node
+    #   node.next = nil
+    # else
+    #   @tail.next = node
+    #   @tail = @tail.next
+    #   @tail.next = nil
+    # end
+    # @lists << node
 
     # add in the correct sort order (haven't been able to put the method together/ check them. )
       # checks to see if the list is a sorted list or not / if there is a list
-      # if (@sort_order != 0 )&& (@count > 0)
-      #   first = @head
-      #   second = @head.next
-      #   new_node = node
-      # only one item in list and new item goes last
-      # elsif (first.object < new_node.object) && (second == nil)
-      #    first.next = new_node
-      #    @tail = new_node
-      #    @tail.next = nil
-      #    end
-      # only one item in list and new item goes first
-      # elsif (first.object < new_node.object)
-      #   @head = new_node
-      #   @head.next = first
-      # this works if the new_node goes between two exisiting nodes (ex[9, 11] --> wanting to add 10)
-    # while second != nil
-    #   until first.object > new_node.object
-    #     previous = first
-    #     first = second
-    #     second = first.next
-    #   end
-    #   previous.next = new_node
-    #   new_node.next = first
-    # end
+
+      if @count == 0
+        @head = node
+        @tail = node
+        node.next = nil
+      elsif (@sort_order == 0 || @sort_order == nil)
+        @tail.next = node
+        @tail = @tail.next
+        @tail.next = nil
+      # end
+      elsif (@sort_order != 0 ) && (@count > 0)
+
+        first = @head
+        second = @head.next
+        new_node = node
+
+        # only one item in list and new item goes last
+        if (first.object < new_node.object) && (second == nil)
+         first.next = new_node
+         @tail = new_node
+         @tail.next = nil
+        # only one item in list and new item goes first
+        elsif (first.object > new_node.object)
+          @head = new_node
+          @head.next = first
+        # this works if the new_node goes between two exisiting nodes (ex[9, 11] --> wanting to add 10)
+        else
+          while second != nil
+            until first.object > new_node.object
+              previous = first
+              first = second
+              second = first.next
+            end
+          previous.next = new_node
+          new_node.next = first
+        end
+      end
+    end
+    @count += 1
+
   end
 
   # enumerable mixin method
   def each(&block)
     return enum_for(:each) unless block_given?
-    @lists.each &block
+    self.each &block
   end
 
   # returns all elements in list
   def elements
-    if @sort_order == 0 || @sort_order == nil
-      to_a
-    else
-      to_a.sort
+    elements = []
+    return nil if @count < 1
+    current = self.head
+
+    while current.next != nil
+      elements << current.object
+      current = current.next
     end
+    elements << current.object
+    # self.head.object
+    # if @sort_order == 0 || @sort_order == nil
+    #   to_a
+    # else
+    #   to_a.sort
+    # end
   end
 
   # returns true if the list is empty, false otherwise
